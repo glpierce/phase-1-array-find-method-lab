181728225dcec5859f7f4c3d02c772b431438019
Grant Pierce <glpierce174@gmail.com>
2021-11-23 13:39:38 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(record) {
+    let year = undefined;
+    let winningSeason = record.find(element => element.result === `W`);
+    if (winningSeason !== undefined) {
+        year = winningSeason.year;
+    }
+    return year;
+}
