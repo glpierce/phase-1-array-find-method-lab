585481604038aa13c86a44989da6abaee3d1cd8d
Grant Pierce <glpierce174@gmail.com>
2021-11-24 11:37:10 -0500

    Made solution more efficient

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function superbowlWin(record) {
-    let year = undefined;
-    let winningSeason = record.find(element => element.result === `W`);
+    const winningSeason = record.find(element => element.result === `W`);
     if (winningSeason !== undefined) {
-        year = winningSeason.year;
+        return winningSeason.year;
+    } else {
+        return undefined;
     }
-    return year;
 }
